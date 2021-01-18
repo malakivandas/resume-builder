c58771e515705e5e08dd12ebccf10600a1ed159d
malakivandas <malakivandas@gmail.com>
2021-01-17 23:16:48 -0800

    Completed backend auth

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) =>
   res.json({ msg: 'Welcome to the Resume Builder API' })
@@ -9,7 +16,7 @@ app.get('/', (req, res) =>
 // Define Routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/resumes', require('./routes/resumes'));
-app.use('/api/users', require('./routes/auth'));
+app.use('/api/users', require('./routes/users'));
 
 const PORT = process.env.PORT || 5000;
 
